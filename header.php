8080fc0391056de15060533c599af2eac6cb5fe5
Patrick Davis <patrick@wholegraindigital.com>
2022-09-15 11:02:05 +0100

    hide cookie notice control button

--- header.php
+++ header.php
@@ -2,13 +2,15 @@
 
 $siteManifest = json_decode(\Granola\Asset::content('static/site.webmanifest'), true);
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
+
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
     <link rel="manifest" href="<?= \Granola\Asset::URL('static/site.webmanifest'); ?>" crossorigin="use-credentials">
-    <meta name="theme-color" content="<?= esc_attr($siteManifest['theme_color']); ?>"/>
+    <meta name="theme-color" content="<?= esc_attr($siteManifest['theme_color']); ?>" />
 
     <?php wp_head(); ?>
 </head>
@@ -16,7 +18,9 @@ $siteManifest = json_decode(\Granola\Asset::content('static/site.webmanifest'), 
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
 
-    <?= \Granola\Component::get('cookie-notice'); ?>
+    <?php if (get_field('show_cookie_notice', 'options')) { ?>
+        <?= \Granola\Component::get('cookie-notice'); ?>
+    <?php } ?>
     <?= \Granola\Component::get('skip-link'); ?>
     <?= \Granola\Component::get('site-header'); ?>
 
